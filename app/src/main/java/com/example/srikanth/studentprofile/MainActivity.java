b280c6893410c4b393beadd0efa33ae8dbd97202
sarathsky <sarathchandra1999@gmail.com>
2017-06-03 19:33:20 +0530

    update at the end of 1st day

--- app/src/main/java/com/example/srikanth/studentprofile/MainActivity.java
+++ app/src/main/java/com/example/srikanth/studentprofile/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.srikanth.studentprofile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton profilePicImage;
+    Button uploadProPicButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onUploadButtonClicked(View view){
+
+        Toast.makeText(MainActivity.this, "hello there", Toast.LENGTH_SHORT).show();
+
+    }
+
+
+
+
 }
