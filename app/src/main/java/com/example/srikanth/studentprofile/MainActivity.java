6aac4400c80a52d840d8e7ed2b363e9c7b99c200
Sarath-SkyRider <kondetisarath@gmail.com>
2017-06-15 17:51:55 +0530

    update from desktop,completed student profile

--- app/src/main/java/com/example/srikanth/studentprofile/MainActivity.java
+++ app/src/main/java/com/example/srikanth/studentprofile/MainActivity.java
@@ -1,17 +1,21 @@
 package com.example.srikanth.studentprofile;
 
+import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.ColorFilter;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.IntRange;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -26,20 +30,31 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static android.R.attr.bitmap;
+import static android.R.attr.thumbnail;
+import static android.os.Environment.getExternalStoragePublicDirectory;
 
 public class MainActivity extends AppCompatActivity {
 
     // This is a reference to catch Profile picture imagebutton view.
     ImageButton profilePicImage;
-    private final static  int SELECTED_PICTURE=1;
+    private final static  int SELECTED_PICTURE_FOR_GALLERY=1;
+    private final static  int CAPTURED_PICTURE=0;
+    String mCurrentPhotoPath;
 
 
     RecyclerView accomRV;
     public static AccomAdapter accomadapter;
+
+    private File imageFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,6 +90,32 @@ public class MainActivity extends AppCompatActivity {
 
                             AlertDialog alertDialog = builder.create();
                             alertDialog.show();
+                        }
+
+                        else if(item.getItemId()==R.id.capture_image_item){
+
+                            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                            // Ensure that there's a camera activity to handle the intent
+                            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                                // Create the File where the photo should go
+                                File photoFile = null;
+                                try {
+                                    photoFile = createImageFile();
+                                } catch (IOException ex) {
+                                    // Error occurred while creating the File
+
+                                }
+                                // Continue only if the File was successfully created
+                                if (photoFile != null) {
+                                    Uri photoURI = FileProvider.getUriForFile(MainActivity.this,
+                                            "com.example.android.fileprovider",
+                                            photoFile);
+                                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                                    startActivityForResult(takePictureIntent, CAPTURED_PICTURE);
+                                }
+                            }
+
+
                         }
 
                         return true;
@@ -140,7 +181,7 @@ public class MainActivity extends AppCompatActivity {
     public void onUploadButtonClicked(){
 
         Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        startActivityForResult(intent,SELECTED_PICTURE);
+        startActivityForResult(intent,SELECTED_PICTURE_FOR_GALLERY);
 
     }
 
@@ -148,7 +189,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if(resultCode == RESULT_OK){
-            if(requestCode == SELECTED_PICTURE){
+            if(requestCode == SELECTED_PICTURE_FOR_GALLERY){
                 Uri image_uri = data.getData();
 
                 InputStream inputStream;
@@ -164,6 +205,34 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(this,"Unable to locate image",Toast.LENGTH_LONG).show();
                 }
             }
+
+
+
+            else if(requestCode == CAPTURED_PICTURE){
+                Toast.makeText(MainActivity.this,"image saved.",Toast.LENGTH_LONG).show();
+                // Get the dimensions of the View
+                int targetW = profilePicImage.getWidth();
+                int targetH = profilePicImage.getHeight();
+
+                // Get the dimensions of the bitmap
+                BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+                bmOptions.inJustDecodeBounds = true;
+                BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+                int photoW = bmOptions.outWidth;
+                int photoH = bmOptions.outHeight;
+
+                // Determine how much to scale down the image
+                int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+                // Decode the image file into a Bitmap sized to fill the View
+                bmOptions.inJustDecodeBounds = false;
+                bmOptions.inSampleSize = scaleFactor;
+                bmOptions.inPurgeable = true;
+
+                Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+                profilePicImage.setImageBitmap(bitmap);
+
+            }
         }
     }
     /*****************************************************************/
@@ -174,5 +243,25 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this,AccomEditActivity.class);
         startActivity(intent);
     }
+
+
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+
 
 }
